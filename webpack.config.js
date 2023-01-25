154402fcec376e45b0dad8d741ba528344d6d65f
Greta Ra <ggretute88@gmail.com>
2023-01-25 15:39:56 +0100

    Configure dev server and add new html file

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+    hot: false,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '#',
-      template: 'src/index.html'
+      title: 'To-do',
+      template: 'src/index.html',
     }),
     new HtmlWebpackPlugin({
-        filename: 'test.html',
-        title: '#',
+        filename: 'today.html',
+        title: 'To-do',
         template: 'src/today.html'
       }),
+    new HtmlWebpackPlugin({
+      filename: 'weekly.html',
+      title: 'To-do',
+      template: 'src/weekly.html'
+    }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
