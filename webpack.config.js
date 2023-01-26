3a0d57ebdab3fb05b5e5bc842c6bed07932270af
Greta Ra <ggretute88@gmail.com>
2023-01-26 13:58:02 +0100

    Add css file, configure webpack support for css and images

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,21 @@ module.exports = {
     static: './dist',
     hot: false,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'To-do',
