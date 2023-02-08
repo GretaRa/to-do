662020f8f8f54a0b3425bb2703d6583faa42254d
Greta Ra <ggretute88@gmail.com>
2023-02-08 16:05:04 +0100

    Add content to modal

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import {createTask} from './task-display';
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let openBtn = document.getElementById("add-task-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let closeBtn = document.getElementsByClassName("close-modal")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+openBtn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeBtn.onclick = function() {
   modal.style.display = "none";
 }
 
